a916ac3d38fdd3f85956931ffbfa0eb4c04a6ab5
Pedro <pedrosilva1944@gmail.com>
2026-09-22 00:43:42 +0100

    adiciona progresso no upload de fotografias

--- static-worker.js
+++ static-worker.js
@@ -1,5 +1,12 @@
 export default {
   async fetch(request, env) {
-    return env.ASSETS.fetch(request);
+    const response = await env.ASSETS.fetch(request);
+    const pathname = new URL(request.url).pathname;
+    if (request.method === 'GET' && (pathname === '/' || pathname === '/index.html')) {
+      const headers = new Headers(response.headers);
+      headers.set('Cache-Control', 'no-store');
+      return new Response(response.body, { status: response.status, statusText: response.statusText, headers });
+    }
+    return response;
   }
 };
